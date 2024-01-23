26ec4cbef84d9c583baf20d1940e24ac8ba83272
JellyBlobbyBrownie <119931258+JellyBlobbyBrownie@users.noreply.github.com>
2024-01-23 19:14:20 +1100

    Update main.js

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ greencar_y=225;
 function add() {
 background_imgTag = new Image();
 background_imgTag.onload = uploadBackground;
-background_imgTag.src - background_image;
+background_imgTag.src = background_image;
 
 greencar_imgTag = new Image();
 greencar_imgTag.onload=uploadgreencar;
